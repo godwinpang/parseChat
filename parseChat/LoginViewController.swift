a8c87067d597e0a5e25070b39febaef20b9bc715
gypang <gypang24@gmail.com>
2018-02-22 02:53:21 -0800

    added login screen with error boxes

--- parseChat/LoginViewController.swift
+++ parseChat/LoginViewController.swift
@@ -11,16 +11,67 @@ import Parse
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var usernameInputField: UITextField!
+    @IBOutlet weak var passwordInputField: UITextField!
+    let signUpUsernameErrorAlertController = UIAlertController(title: "Username Required", message: "Please enter username", preferredStyle: .alert)
+    let signUpPasswordErrorAlertController = UIAlertController(title: "Password Required", message: "Please enter password", preferredStyle: .alert)
+    let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
+        //does nothing -> dismisses alert view
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        signUpUsernameErrorAlertController.addAction(OKAction)
+        signUpPasswordErrorAlertController.addAction(OKAction)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func registerUser(_ sender: Any) {
+        let newUser = PFUser()
+        
+        newUser.username = usernameInputField.text
+        newUser.password = passwordInputField.text
+        
+        if (usernameInputField.text?.isEmpty)!{
+            present(signUpUsernameErrorAlertController, animated: true)
+        } else if (passwordInputField.text?.isEmpty)!{
+            present(signUpPasswordErrorAlertController, animated: true)
+        } else {
+            newUser.signUpInBackground { (success: Bool, error: Error?) in
+                print("User Registered successfully")
+                if let error = error {
+                    let signUpErrorAlertController = UIAlertController(title: "Signup Failed", message: "\(error.localizedDescription)", preferredStyle: .alert)
+                    signUpErrorAlertController.addAction(self.OKAction)
+                    //self.present(signUpErrorAlertController, animated: true)
+                } else {
+                    print("User Registered successfully")
+                    //self.performSegue(withIdentifier: "loginSegue", sender: nil)
+                }
+            }
+        }
+    }
+    
+    @IBAction func loginUser(_ sender: Any) {
+        let username = usernameInputField.text ?? ""
+        let password = passwordInputField.text ?? ""
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user: PFUser?, error: Error?) in
+            print("User logged in successfully")
+            if let error = error {
+                let loginErrorAlertController = UIAlertController(title: "Login Failed", message: "\(error.localizedDescription)", preferredStyle: .alert)
+                loginErrorAlertController.addAction(self.OKAction)
+                self.present(loginErrorAlertController, animated: true)
+            } else {
+                print("User logged in successfully")
+                //self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+        }
+    }
+    
+    
 }
 
